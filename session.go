9a0acdb09fcf72a5fd847110874554ada3cb68c0
ritika29 <setritika16@gmail.com>
2018-08-23 14:54:24 +0530

    sesion

---
+++ session.go
@@ -0,0 +1,35 @@
+package main
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/sessions"
+)
+var (
+	key = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
+)
+func secret(w http.ResponseWriter,r *http.Request) {
+	session,_ := store.Get(r,"cookie-name")
+	if auth,ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w,"forbidden",http.StatusForbidden)
+		return
+	}
+	fmt.Fprintln(w,"the cake is a lie")
+}
+func login(w http.ResponseWriter,r *http.Request) {
+	session,_ := store.Get(r,"cookie-name")
+	session.Values["authenticated"] = true
+     session.Save(r,w)
+}
+func logout(w http.ResponseWriter,r *http.Request) {
+	session,_ := store.Get(r,"cookie-name")
+	session.Values["authenticated"] = false
+    session.Save(r,w)
+}
+func main() {
+	fmt.Printf("server on")
+	http.HandleFunc("/secret",secret)
+	http.HandleFunc("/login",login)
+	http.HandleFunc("logout",logout)
+	http.ListenAndServe(":7890",nil)
+}
